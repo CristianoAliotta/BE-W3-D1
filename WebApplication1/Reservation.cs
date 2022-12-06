b92b5b9f4859b2b45c3c6303cdf22a222d709c0c
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-12-06 15:08:11 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Prenotazione
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Sala { get; set; }
+        public bool BigliettoRidotto { get; set; }
+
+        public static List<Prenotazione> ListaPrenotazione { get; set; } = new List<Prenotazione>();
+    }
+}
